cee6412d82e3ac6fa9f12d87330dbc4f44363268
Mauricio <conta.alonso@gmail.com>
2022-07-06 23:13:18 -0300

    Terminé ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,14 @@
+/* 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+*/
+
+let edad = parseInt(prompt("Ingresar edad: "));
+
+if(edad >=18){
+    document.write("Usted ya puede solicitar licencia para conducir");
+}
+else if (edad<18) {
+    document.write("No tiene edad legal para solicitar la licencia.")
+}
+else{
+    document.write ("El texto ingresado no es un caracter válido.")
+}
