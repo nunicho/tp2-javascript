01ade133bac9987f3f9f1ae0642d7384f137f412
Mauricio <conta.alonso@gmail.com>
2022-07-10 01:37:43 -0300

    Terminé ejercicio 10.

---
+++ js/ejercicio10.js
@@ -0,0 +1,29 @@
+/* 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+*/
+
+let fila =parseInt(prompt("Inserte el número de filas deseado: "))
+let columna =parseInt(prompt("Inserte el número de columnas deseado: "))
+let numeroCelda = fila*columna;
+
+const createTable = (row, col) => {
+    let body = document.body;
+    let tbl = document.createElement("table");
+  
+    for (let i = 0; i < row; i++) {
+        let tr = tbl.insertRow();
+      for (let j = 0; j < col; j++) {
+        let td = tr.insertCell();
+        td.appendChild(document.createTextNode(numeroCelda--));
+        td.style.border = "1px solid black";
+      }
+    }
+    body.appendChild(tbl);
+  };
+
+  createTable(fila, columna);
+
+
+/*
+  Investigué en 
+  https://stackoverflow.com/questions/14643617/create-table-using-javascript
+*/
